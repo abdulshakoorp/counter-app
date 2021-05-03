9a320be991e83386580f3f028d60696d645d7823
Abdul Shakoor <abdulshakoorp@hotmail.com>
2021-05-03 17:35:19 +0400

    navbar and state transfer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
+import { render } from "@testing-library/react";
+import { Component } from "react";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 0 },
+      { id: 4, value: 3 },
+    ],
+  };
+
+  handleReset = (counterId) => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((d) => d.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    //console.log(this.state.counters[index]);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCounters={this.state.counters.filter((d) => d.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,39 @@
-import React, { Component } from "react";
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar
-            </a>
-          </div>
-        </nav>
-      </>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {props.totalCounters}
+            </span>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+// the above code is state less, that is why its created as function
+// import React, { Component } from "react";
+// class Navbar extends Component {
+//   state = {};
+//   render() {
+//     return (
+//       <>
+//         <nav className="navbar navbar-light bg-light">
+//           <div className="container-fluid">
+//             <a className="navbar-brand" href="#">
+//               Navbar{" "}
+//               <span className="badge badge-pill badge-secondary">
+//                 {this.props.totalCounters}
+//               </span>
+//             </a>
+//           </div>
+//         </nav>
+//       </>
+//     );
+//   }
+// }
 
 export default Navbar;
