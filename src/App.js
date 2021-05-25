29467c80f642ab2f63cccd00f5910b64909f2fd2
Abdul Shakoor <abdulshakoorp@hotmail.com>
2021-05-25 10:35:30 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       { id: 1, value: 1 },
       { id: 2, value: 2 },
       { id: 3, value: 0 },
-      { id: 4, value: 3 },
+      { id: 4, value: 6 },
     ],
   };
 
